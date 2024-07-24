21007bd4c08bfd7ed14898837e198526dcba63d6
koalasat <koalasat@satstralia.com>
2024-07-24 18:11:03 +0200

    Tesnet sync

--- compose/strfry/tn.sync.sh
+++ compose/strfry/tn.sync.sh
@@ -2,5 +2,4 @@
 
 filters='{"kinds":[38383]}'
 
-/app/strfry --config /etc/strfry.conf sync wss://nostr.satstralia.com --filter "$filters" --dir both >> /var/log/cron.log 2>&1
 /app/strfry --config /etc/strfry.conf sync ws://testraliar7xkhos2gipv2k65obykofb4jqzl5l4danfryacifi4t7qd.onion/nostr --filter "$filters" --dir both >> /var/log/cron.log 2>&1

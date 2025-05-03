f419bfc87232843a9f541d6ca0b87bc49a54cf67
koalasat <koalasat@satstralia.com>
2025-05-03 18:14:01 +0200

    Back to extnerl

--- compose/strfry/sync.sh
+++ compose/strfry/sync.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 filters_federation='{"kinds":[38383, 31986, 1059]}'
+timeout_duration="10s"
 
 while IFS= read -r line; do
-  /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_federation" --dir both
+  timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_federation" --dir both
 done < /app/federation_urls.txt
 
 
 filters_external='{"kinds":[38383]}'
 
 while IFS= read -r line; do
-  /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_external" --dir both
+  timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_external" --dir both
 done < /app/external_urls.txt

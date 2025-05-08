644a79ee4ff30254a4c9e4d1d6092a60d88df168
koalasat <koalasat@satstralia.com>
2025-05-08 16:49:22 +0200

    Use new relay
    Conflicts:
    	compose/relay/sync_external.sh
    	compose/strfry/sync.sh

--- compose/strfry/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-filters='{"kinds":[38383, 31986, 1059]}'
-
-while IFS= read -r line; do
-  /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters" --dir both
-done < /app/onion_urls.txt

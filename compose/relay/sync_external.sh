1f6d1e5c467a38f8fe4d688c0e4b9d24e1742ada
KoalaSat <git@koalasat.xyz>
2026-01-26 13:23:07 +0100

    Auto-delete old nostr events

--- compose/relay/sync_external.sh
+++ compose/relay/sync_external.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-filters_external='{"kinds":[38383]}'
+filters_sync="{\"kinds\":[38383],\"since\":$(date -d "2 weeks ago" +%s)}"
 timeout_duration="15s"
 
 while IFS= read -r line; do
-  timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_external" --dir both
+  timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_sync" --dir both
 done < /app/external_urls.txt
--- compose/relay/sync_federation.sh
+++ compose/relay/sync_federation.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-filters_federation='{"kinds":[38383, 31986, 1059]}'
+filters_sync="{\"kinds\":[38383,31986,1059],\"since\":$(date -d "4 weeks ago" +%s)}"
+filter_delete="{\"until\":$(date -d "4 weeks ago" +%s)}"
 timeout_duration="15s"
 
+timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf delete --filter "$filter_delete"
+
 while IFS= read -r line; do
-  timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_federation" --dir both
+  timeout "$timeout_duration" /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters_sync" --dir both
 done < /app/federation_urls.txt
